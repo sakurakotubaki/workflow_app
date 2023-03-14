afe59dfecdf1189b221003e76fd3ca2f331f3b97
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-03-14 22:05:39 +0900

    add ignore: dead_code

--- lib/redirect.dart
+++ lib/redirect.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: prefer_const_declarations
+
 class RouterTest {
   static isAuthState() {
     final isAuth = true;
+    // ↓無視するという意味
+    // ignore: dead_code
     if (!isAuth) {
       return 'HomePage';
     } else {
