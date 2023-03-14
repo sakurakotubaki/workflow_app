d2f7a3f93595cb4d162969e318fc1dc423e602e7
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-03-14 22:31:41 +0900

    add test code3

--- test/unit_test/redirect_test.dart
+++ test/unit_test/redirect_test.dart
@@ -1,13 +1,19 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, unused_local_variable
 import 'dart:math';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:workflow_app/redirect.dart';
 
 void main() {
-  test('成功するテスト', () {
+  test('LoginPageへリダイレクトするテスト', () {
     final result = RouterTest.isAuthState();
 
     expect(result, 'LoginPage');
+  });
+
+  test('ユーザー情報がnullだったら、CreateUserPageへリダイレクトするテスト', () {
+    final result = RouterTest.isUser();
+
+    expect(result, 'CreateUserPage');
   });
 }
